4e50fa6bbfb45d28f8eb7224c2e0af2068b50958
khushbu2002 <khushbukoul2@gmail.com>
2026-05-15 17:28:30 +0530

    add color change project

---
+++ script.js
@@ -0,0 +1,45 @@
+const buttons =document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+// buttons.forEach(function (button){
+//     console.log(button);
+//     button.addEventListener('click', function (e) {
+//         console.log(e);
+//         console.log(e.target);
+//         // console.log(e.target.id);
+//         if (e.target.id === 'white') {
+//             body.style.backgroundColor = e.target.id;
+//         }
+
+//          if (e.target.id === 'pink') {
+//             body.style.backgroundColor = e.target.id;
+//         }
+
+//          if (e.target.id === 'yellow') {
+//             body.style.backgroundColor = e.target.id;
+//         }
+
+//          if (e.target.id === 'green') {
+//             body.style.backgroundColor = e.target.id;
+//         }
+//     });
+// });
+
+// buttons.forEach(function(button){
+
+//    button.addEventListener('click', function(e) {
+
+//       body.style.backgroundColor = e.target.id;
+//    });
+
+// });
+
+buttons.forEach(function(button) {
+
+   button.addEventListener('click', function(e) {
+
+      body.style.backgroundColor = e.target.id;
+
+   });
+
+});
